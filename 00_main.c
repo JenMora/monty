a5865eb2b295298b344aaa1fe7a26c5b660671ef
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-27 23:47:14 +0300

    test 3

--- 00_main.c
+++ 00_main.c
@@ -1,30 +1,19 @@
 #include "monty.h"
 
-char **token_operators= NULL;
-
-/**
- * main - the entry
- * @argc: parameter
- * @argv: parameter
- * Return: success or failure of exit
- */
 int main(int argc, char **argv)
 {
 	FILE *fileDir= NULL;
 	int mj_exit= EXIT_SUCCESS;
 
 	if (argc != 2)
-	{
 		return (montyRunError());
-	}
+
 	fileDir= fopen(argv[1], "r");
-
 	if (fileDir== NULL)
-	{
 		return (fileOpenError(argv[1]));
-	}
-	mj_exit= letsRunMonty(fileDir);
 
+	mj_exit= letsRunMonty(fileDir);
 	fclose(fileDir);
+
 	return (mj_exit);
 }
