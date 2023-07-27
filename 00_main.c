8e0a1d43f6e9d81bb52bdf95345e5c723e597d35
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-28 01:00:15 +0300

    test 5

--- 00_main.c
+++ 00_main.c
@@ -1,6 +1,16 @@
 #include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 char **token_operators = NULL;
 
+/**
+ * main - entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: success or failure
+ */
 int main(int argc, char **argv)
 {
 	FILE *fileDir= NULL;
@@ -9,7 +19,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		return (montyRunError());
 
-	fileDir= fopen(argv[1], "r");
+	fileDir = fopen(argv[1], "r");
 	if (fileDir == NULL)
 		return (fileOpenError(argv[1]));
 
